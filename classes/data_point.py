0605a4d67ced6cc8500b84b3ba45aacbe1ea4699
amande <31880536+amandinecaut@users.noreply.github.com>
2024-09-10 13:01:37 +0200

    Update data_point.py
    Personality project

--- classes/data_point.py
+++ classes/data_point.py
@@ -25,3 +25,26 @@ class Player(Stat):
         self.relevant_metrics = relevant_metrics
         # Save metrics as a Series
         self.ser_metrics = ser_metrics
+
+class PersonalityStats(Stat):
+    data_point_class = data_point.Person
+    
+    def __init__(self):
+        super().__init__()
+
+    def get_raw_data(self):
+        df = pd.read_csv("data/events/dataset.csv",encoding='unicode_escape')
+        return df
+
+    def to_data_point(self,name,extraversion,neurotiscism,agreeableness,conscientiousness,openness) -> data_point.Person:
+        
+        id = self.df.index
+        name = self.name
+        extraversion = self.extraversion
+        neurotiscism = self.neurotiscism
+        agreeableness = self.agreeableness
+        conscientiousness = self.conscientiousness
+        openness = self.openness
+
+        
+        return self.data_point_class(id=id,name=name, extraversion=extraversion,neurotiscism=neurotiscism,agreeableness=agreeableness,conscientiousness=conscientiousness,openness=openness)
