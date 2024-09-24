0b4bb2502b8e3e2199fa5137f437164777352632
AmyRouillard <rouillardamy@gmail.com>
2024-09-24 12:00:57 +0200

    Intial version of world value survey page, chat not yet implimented

--- classes/data_source.py
+++ classes/data_source.py
@@ -7,7 +7,7 @@ import copy
 import json
 import datetime
 from scipy.stats import zscore
-import os 
+import os
 
 from itertools import accumulate
 from pathlib import Path
@@ -16,14 +16,16 @@ import pyarrow.parquet as pq
 
 
 import classes.data_point as data_point
-#from classes.wyscout_api import WyNot
+
+# from classes.wyscout_api import WyNot
 
 
-# Base class for all data 
-class Data():
+# Base class for all data
+class Data:
     """
     Get, process, and manage various forms of data.
     """
+
     data_point_class = None
 
     def __init__(self):
@@ -33,17 +35,19 @@ class Data():
         raise NotImplementedError("Child class must implement get_raw_data(self)")
 
     def process_data(self, df_raw: pd.DataFrame) -> pd.DataFrame:
-        raise NotImplementedError("Child class must implement process_data(self, df_raw)")
+        raise NotImplementedError(
+            "Child class must implement process_data(self, df_raw)"
+        )
 
     def get_processed_data(self):
 
         raw = self.get_raw_data()
         return self.process_data(raw)
-    
+
     def select_and_filter(self, column_name, label):
-        
+
         df = self.df
-        selected_id = st.sidebar.selectbox(label, df[column_name].unique())
+        selected_id = st.selectbox(label, df[column_name].unique())
         self.df = df[df[column_name] == selected_id]
 
 
@@ -62,8 +66,8 @@ class Stats(Data):
         self.metrics = []
         self.negative_metrics = []
 
-    def get_metric_zscores(self,df):
- 
+    def get_metric_zscores(self, df):
+
         df_z = df.apply(zscore, nan_policy="omit")
 
         # Rename every column to include "Z" at the end
@@ -74,7 +78,7 @@ class Stats(Data):
             df_z[metric] = df_z[metric] * -1
         return df_z
 
-    def get_ranks(self,df):
+    def get_ranks(self, df):
         df_ranks = df.rank(ascending=False)
 
         # Rename every column to include "Ranks" at the end
@@ -82,18 +86,18 @@ class Stats(Data):
 
         return df_ranks
 
-    def get_pct_ranks(self,df):
+    def get_pct_ranks(self, df):
         df_pct = df.rank(pct=True) * 100
         # Rename every column to include "Pct_Ranks" at the end
         df_pct.columns = [f"{col}_Pct_Ranks" for col in df_pct.columns]
 
         return df_pct
 
-    def calculate_statistics(self,metrics,negative_metrics=[]):
-        self.metrics=metrics
-        self.negative_metrics=negative_metrics
-        
-        df=self.df
+    def calculate_statistics(self, metrics, negative_metrics=[]):
+        self.metrics = metrics
+        self.negative_metrics = negative_metrics
+
+        df = self.df
         # Add zscores, rankings and qualities
         df_metric_zscores = self.get_metric_zscores(df[metrics])
         # Here we want to use df_metric_zscores to get the ranks and pct_ranks due to negative metrics
@@ -101,7 +105,7 @@ class Stats(Data):
 
         # Add ranks and pct_ranks as new columns
         self.df = pd.concat([df, df_metric_zscores, df_metric_ranks], axis=1)
-        
+
 
 class PlayerStats(Stats):
     data_point_class = data_point.Player
@@ -113,10 +117,9 @@ class PlayerStats(Stats):
 
         super().__init__()
 
-
     def get_raw_data(self):
 
-        df = pd.read_csv("data/events/Forwards.csv",encoding='unicode_escape')
+        df = pd.read_csv("data/events/Forwards.csv", encoding="unicode_escape")
 
         return df
 
@@ -132,20 +135,76 @@ class PlayerStats(Stats):
 
         return df_raw
 
-    def to_data_point(self,gender,position) -> data_point.Player:
-        
+    def to_data_point(self, gender, position) -> data_point.Player:
+
         id = self.df.index[0]
 
-        #Reindexing dataframe
+        # Reindexing dataframe
         self.df.reset_index(drop=True, inplace=True)
 
-        name=self.df['player_name'][0]
-        minutes_played=self.df['Minutes'][0]
-        self.df=self.df.drop(columns=["player_name", "Minutes"])
+        name = self.df["player_name"][0]
+        minutes_played = self.df["Minutes"][0]
+        self.df = self.df.drop(columns=["player_name", "Minutes"])
 
         # Convert to series
         ser_metrics = self.df.squeeze()
-        
-        return self.data_point_class(id=id,name=name,minutes_played=minutes_played,gender=gender,position=position,ser_metrics=ser_metrics,relevant_metrics=self.metrics)
 
-    
+        return self.data_point_class(
+            id=id,
+            name=name,
+            minutes_played=minutes_played,
+            gender=gender,
+            position=position,
+            ser_metrics=ser_metrics,
+            relevant_metrics=self.metrics,
+        )
+
+
+class CountryStats(Stats):
+    data_point_class = data_point.Country
+    # This can be used if some metrics are not good to perform, like tackles lost.
+    negative_metrics = []
+
+    def __init__(self):
+
+        self.core_value_dict = pd.read_csv("data/wvs/values.csv")
+        # convert to dictionary using the value and name columns
+        self.core_value_dict = self.core_value_dict.set_index("value")["name"].to_dict()
+
+        super().__init__()
+
+    def get_raw_data(self):
+
+        df = pd.read_csv("data/wvs/wave0.csv")
+
+        return df
+
+    def process_data(self, df_raw):
+        df_raw = df_raw.rename(columns=self.core_value_dict)
+
+        # df_raw = df_raw.replace({-1: np.nan})
+
+        if len(df_raw) < 10:  # Or else plots won't work
+            raise Exception("Not enough players with enough minutes")
+
+        return df_raw
+
+    def to_data_point(self) -> data_point.Country:
+
+        id = self.df.index[0]
+
+        # Reindexing dataframe
+        self.df.reset_index(drop=True, inplace=True)
+
+        name = self.df["country"][0]
+        self.df = self.df.drop(columns=["country"])
+
+        # Convert to series
+        ser_metrics = self.df.squeeze()
+
+        return self.data_point_class(
+            id=id,
+            name=name,
+            ser_metrics=ser_metrics,
+            relevant_metrics=self.metrics,
+        )
--- pages/wvs_chat.py
+++ pages/wvs_chat.py
@@ -1,22 +1,41 @@
 # Library imports
 import streamlit as st
-import pandas as pd
-import argparse
-import tiktoken
-import os
-from utils.utils import normalize_text
+from utils.utils import select_country, create_chat
 
-from classes.data_source import PlayerStats
-from classes.data_point import Player
+from classes.data_source import CountryStats
 
+# from classes.chat import WVSChat
 
 from utils.page_components import add_common_page_elements
 
+
+# def show():
 sidebar_container = add_common_page_elements()
 page_container = st.sidebar.container()
 sidebar_container = st.sidebar.container()
 
 st.divider()
 
+countries = CountryStats()
 
-st.write("Under construction")
+metrics = [m for m in countries.df.columns if m not in ["country"]]
+
+countries.calculate_statistics(metrics=metrics)
+
+country = select_country(sidebar_container, countries)
+
+st.write(
+    "This app can only handle three or four users at a time. Please [download](https://github.com/soccermatics/twelve-gpt-educational) and run on your own computer with your own Gemini key."
+)
+
+st.expander("Dataframe used", expanded=False).write(countries.df)
+
+
+# Chat state hash determines whether or not we should load a new chat or continue an old one
+# We can add or remove variables to this hash to change conditions for loading a new chat
+to_hash = (country.id,)
+
+# chat = create_chat(to_hash, WVSChat, country, countries)
+
+
+# st.write("Under construction")
