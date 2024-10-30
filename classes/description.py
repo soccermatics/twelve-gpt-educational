3435aa46df94cab8a904389dda0516e5061c4723
AmyRouillard <rouillardamy@gmail.com>
2024-10-30 12:59:45 +0100

    model card updates

--- classes/description.py
+++ classes/description.py
@@ -188,7 +188,6 @@ class Description(ABC):
             converted_msgs = convert_messages_format(self.messages)
 
             # # save converted messages to json
-            # import json
             # with open("data/wvs/msgs_0.json", "w") as f:
             #     json.dump(converted_msgs, f)
 
